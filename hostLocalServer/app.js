b158cc8235a689582c7f51bcb59a30ac0f751355
Devaa <48400805+DevaassenanMahenthiran@users.noreply.github.com>
2019-12-30 11:10:44 +0800

    4th Task for Google Code in 2019
    Task Name: [Python/Java/C++/Perl/PHP/Rust] Create and host a local server - SCoRe Lab

---
+++ hostLocalServer/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/", require("./routes/main.router"));
+app.use(express.static("public"));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server running on Port ${PORT}`)
+})
